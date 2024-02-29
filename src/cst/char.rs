3a82818920d7fab0359390ba563b39f76e2d0e5a
pulk66-s <hugo.marrasse@epitech.eu>
2024-02-29 18:06:56 +0000

    add of nested +

--- src/cst/char.rs
+++ src/cst/char.rs
@@ -2,9 +2,29 @@ use either::Either::{self, Left, Right};
 
 use super::data::CstAtom;
 
-pub fn create_cst_add_atom(expr: &str) -> Either<&str, (CstAtom, &str)> {
+fn create_cst_char_atom(expr: &str, c: char) -> Either<&str, (CstAtom, &str)> {
     match expr.chars().next() {
-        Some('+') => Right((CstAtom::CHAR('+'), &expr[1..])),
+        Some(ch) if ch == c => Right((CstAtom::CHAR(c), &expr[1..])),
         _ => Left(expr)
     }
+}
+
+pub fn create_cst_add_atom(expr: &str) -> Either<&str, (CstAtom, &str)> {
+    create_cst_char_atom(expr, '+')
+}
+
+pub fn create_cst_sub_atom(expr: &str) -> Either<&str, (CstAtom, &str)> {
+    create_cst_char_atom(expr, '-')
+}
+
+pub fn create_cst_mul_atom(expr: &str) -> Either<&str, (CstAtom, &str)> {
+    create_cst_char_atom(expr, '*')
+}
+
+pub fn create_cst_div_atom(expr: &str) -> Either<&str, (CstAtom, &str)> {
+    create_cst_char_atom(expr, '/')
+}
+
+pub fn create_cst_endexpr_atom(expr: &str) -> Either<&str, (CstAtom, &str)> {
+    create_cst_char_atom(expr, ';')
 }
--- src/cst/expr.rs
+++ src/cst/expr.rs
@@ -5,9 +5,16 @@ use crate::cst::{
     data::CstNode
 };
 
+use super::binop::create_cst_binop;
+
 pub fn create_cst_value_expr(expr: &str) -> Either<&str, (CstNode, &str)> {
+    match create_cst_binop(expr) {
+        Left(_) => {},
+        Right((binop, new_expr)) => return Right((binop, new_expr)),
+    };
     match create_cst_number(expr) {
-        Left(new_expr) => Left(new_expr),
-        Right((atom, new_expr)) => Right((CstNode::ATOM(atom), new_expr)),
-    }
+        Left(_) => {},
+        Right((atom, new_expr)) => return Right((CstNode::ATOM(atom), new_expr)),
+    };
+    Left("create_cst_value_expr: no match found.")
 }
