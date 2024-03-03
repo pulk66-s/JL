7d4f0441906f7968c8e54350ecc1647b3e9c35f8
pulk66-s <hugo.marrasse@epitech.eu>
2024-03-03 10:41:53 +0000

    handle of multiline

--- src/cst/keyword.rs
+++ src/cst/keyword.rs
@@ -10,6 +10,8 @@ pub fn create_cst_spaces(expr: &str) -> Either<&str, (CstAtom, &str)> {
 
         match first_char {
             Some(' ') => new_expr = &new_expr[1..],
+            Some('\n') => new_expr = &new_expr[1..],
+            Some('\t') => new_expr = &new_expr[1..],
             _ => return Right((CstAtom::CHAR(' '), new_expr)),
         }
     }
